8477a27d8d1f26a3809b940e6d377d9c61f84f45
AbdulBasit0044 <abdulbasit0044@gmail.com>
2021-07-23 06:52:38 +0500

    Update index.js

--- Good Stuff/index.js
+++ Good Stuff/index.js
@@ -1,8 +1,13 @@
+
+console.log("i have to make it correct");
 // Print the area of the circle:
 //readLine to fet
 const PI = Math.PI;
-let r = readLine();
+const readline = require('readline');
+let r = readine();
 console.log(PI*r**2);
 
 //array.includes(machingValue);
-'aeiou'.includes(s[0]):
+// 'aeiou'.includes(s[0]):
+
+
